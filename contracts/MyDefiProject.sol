16c5e3236558cf20f05416f753c0c674d5e86244
brahmavihara <brahmavihara@protonmail.com>
2021-11-26 09:58:52 +0900

    compound integration

--- contracts/MyDefiProject.sol
+++ contracts/MyDefiProject.sol
@@ -1,47 +1,43 @@
 pragma solidity ^0.7.3;
 
-interface IUniswap {
-  function swapExactTokensForETH(
-    uint amountIn, 
-    uint amountOutMin, 
-    address[] calldata path, 
-    address to, 
-    uint deadline)
-    external
-    returns (uint[] memory amounts);
-  function WETH() external pure returns (address);
-}
-
-interface IERC20 {
-  function transferFrom(
-    address sender, 
-    address recipient, 
-    uint256 amount) 
-    external 
-    returns (bool);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./CTokenInterface.sol";
+import "./ComptrollerInterface.sol";
+import "./PriceOracleInterface.sol";
 
 contract MyDeFiProject {
-  IUniswap uniswap;
+    ComptrollerInterface public comptroller;
+    PriceOracleInterface public priceOracle;
 
-  constructor(address _uniswap) {
-    uniswap = IUniswap(_uniswap);
-  }
+    constructor(
+        address _comptroller,
+        address _priceOracle
+    ) {
+        comptroller = ComptrollerInterface(_comptroller);
+        priceOracle = PriceOracleInterface(_priceOracle);
+    }
+    //Pattern of passing in.
 
-  function swapTokensForEth(address token, uint amountIn, uint amountOutMin, uint deadline) external {
-    IERC20(token).transferFrom(msg.sender, address(this), amountIn);
-    address[] memory path = new address[](2);
-    path[0] = address(DAI);
-    path[1] = uniswap.WETH();
-    IERC20(token).approve(address(uniswap), amountIn);
-    uniswap.swapExactTokensForETH(
-      amountIn, 
-      amountOutMin, 
-      path, 
-      msg.sender, 
-      deadline
-    );
-  }
+    function supply(address cTokenAddress, uint underlyingAmount) external {
+        CTokenInterface cToken = CTokenInterface(CTokenInterface);
+        address underlyingAddress = cToken.underlying();
+        IERC20(underlyingAddress).approve(cTokenAddress, underlyingAmount);
+        uint result = cToken.mint(underlyingAmount);
+        require(
+            result == 0,
+            "cToken#mint() failed. See Compound ErrorReport.sol for more details"
+        );
+    }
+
+    function redeem(address cTokenAddress, uint cTokenAmount) external {
+        CTokenInterface cToken = CTokenInterface(cTokenAddress);
+        uint result = cToken.redeem(cTokenAmount);
+        require(
+            result == 0,
+            "cToken#redeem() failed. See Compound ErrorReport.sol for more details"
+        );
+
+    //Rest, functions are similar (enter, borrow, repay, getMax)
+    }
 }
-
-//Need analyzing and practice.
+//Need a lot more work here.
